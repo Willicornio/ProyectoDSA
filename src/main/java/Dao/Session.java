6ec59b8bedae1e0e3eb92f43aa446f999c60ab5d
Willicornio <guillecacerola@gmail.com>
2019-05-09 16:25:11 +0200

    session con select

--- src/main/java/Dao/Session.java
+++ src/main/java/Dao/Session.java
@@ -1,34 +1,27 @@
 package Dao;
 
-import java.beans.IntrospectionException;
 import java.beans.PropertyDescriptor;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.security.PublicKey;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.Date;
-import java.util.logging.Logger;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Session {
 
-    Logger log = Logger.getLogger(Session.class.getName());
     Connection session = null;
     Statement statement = null;
     PreparedStatement preparedStatement = null;
     ResultSet resultSet = null;
 
-    protected Session () {
+    protected Session() {
 
         try {
             this.session = getConnection();
 
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
@@ -51,48 +44,275 @@ public class Session {
         }
 
 
-
     }
 
-    public void save (Object o) throws SQLException, IntrospectionException, InvocationTargetException, IllegalAccessException {
+
+    public void close() throws Exception {
+        session.close();
+    }
+
+
+    public void save(Object o) throws Exception {
+
+
         statement = session.createStatement();
         String preparedQuery = null;
         String query = null;
+        // Result set get the result of the SQL query
+        String table;
         int contador = 1;
+        table = o.getClass().getSimpleName();
 
-        String table = o.getClass().getSimpleName();
-        log.info(table);
+        System.out.println(table);
+
+
         Field[] fields = o.getClass().getDeclaredFields();
 
 
-        for(int i = 1; i <= fields.length; i++){
-            if(i == 1 || i != fields.length){
+        for (int i = 1; i <= fields.length; i++) {
+
+            if (i == 1) {
+
                 preparedQuery = "?,";
-            }else {
-                preparedQuery += "?";
+
             }
+            if (i != fields.length & i != 1) {
+
+                preparedQuery += "?,";
+            }
+            if (i == fields.length)
+                preparedQuery += "?";
         }
 
-        query = "INSERT INTO " + table + " VALUES (" + preparedQuery +")";
-        log.info(query);
 
-        PreparedStatement ps =  this.session.prepareStatement(query);
+        query = "INSERT INTO " + table + " VALUES (" + preparedQuery + ")";
+        System.out.println(query);
+        for (Field f : fields) {
+            System.out.println(fields.length);
+        }
 
-        for(Field f : fields){
-            ps.setString(contador, new PropertyDescriptor(fields[contador - 1].getName(),o.getClass()).getReadMethod().invoke(o).toString() );
+        PreparedStatement ps = this.session.prepareStatement(query);
+
+        for (Field f : fields) {
+            ps.setString(contador, new PropertyDescriptor(fields[contador - 1].getName(), o.getClass()).getReadMethod().invoke(o).toString());
             contador++;
 
         }
 
         ps.executeUpdate();
-        log.info("OK!");
         ps.close();
-
     }
 
+
+    /*if ( a == "Usuario"){
+
+        String nombre;
+
+
+
+        e.getName();
+
+        preparedStatement = session
+                .prepareStatement("insert into  feedback." + a +"values (default, ?, ?)");
+        // "myuser, webpage, datum, summary, COMMENTS from feedback.comments");
+        // Parameters start with 1
+        preparedStatement.setString(1, Nombre);
+        preparedStatement.setString(2, apellido);
+        preparedStatement.executeUpdate();
+    }
+
+
+}catch (Exception fatal){
+    throw fatal;
+}
+
+
+}
 
 
 
 
 }
+
+
+
+
+
+
+*/
+    public void update ( Object o, String id) throws Exception {
+
+        statement = session.createStatement();
+        String preparedQuery = null;
+        String query = null;
+        // Result set get the result of the SQL query
+        String table;
+        int contador = 1;
+        table = o.getClass().getSimpleName();
+
+        System.out.println(table);
+
+
+        Field[] fields = o.getClass().getDeclaredFields();
+
+
+        for (int i = 1; i <= fields.length; i++) {
+
+            if (i == 1) {
+
+                preparedQuery = "? = ?,";
+
+            }
+            if (i != fields.length & i != 1) {
+
+                preparedQuery += "? = ?,";
+            }
+            if (i == fields.length)
+                preparedQuery += "? =? ";
+        }
+
+        query = "UPDATE " + table + " SET nombre = caracaola WHERE ( id = " + id + " )";
+
+        // UPDATE `feedback`.`usuario` SET `nombre` = 'este' WHERE (`id` = 'Willi25');
+
+
+        query = "UPDATE " + table + " SET (" + preparedQuery + ") WHERE id =" + id;
+        System.out.println(query);
+        for (Field f : fields) {
+            System.out.println(fields.length);
+        }
+
+        PreparedStatement ps = this.session.prepareStatement(query);
+        contador = (fields.length-1)*2;
+        for (int cosa = 0; cosa < contador; cosa ++) {
+
+
+            if (contador%2 == 0)
+                ps.setString(contador, new PropertyDescriptor(fields[contador +1].getName(), o.getClass()).getReadMethod().invoke(o).toString());
+
+            if (contador%2 != 0){
+                ps.setString(contador, new PropertyDescriptor(fields[contador +1].getName(), o.getClass()).getName().toString());
+            }
+            contador = contador +1;
+
+
+            ps.executeUpdate();
+            ps.close();
+
+
+        }
+    }
+
+
+    public Object get( String id, Class clase) throws Exception {
+
+        Object o = clase.newInstance();
+        String table = o.getClass().getSimpleName();
+        statement = session.createStatement();
+        String preparedQuery = null;
+        String query = null;
+        int contador = 1;
+
+
+        Field[] fields = o.getClass().getDeclaredFields();
+
+        for (int i = 1; i <= fields.length; i++) {
+            if (i == 1 || i != fields.length) {
+                preparedQuery = "?,";
+            } else {
+                preparedQuery += "?";
+            }
+        }
+
+        query = "SELECT " + preparedQuery + " FROM " + table + "WHERE id =" + id;
+
+
+        PreparedStatement ps = this.session.prepareStatement(query);
+/*
+        for(Field f : fields){
+            ps.setString(contador, new PropertyDescriptor(fields[contador - 1].getName(),o.getClass()).getReadMethod().invoke(o).toString() );
+            contador++;
+
+        }*/
+
+        resultSet = ps.executeQuery();
+
+        o = writeResultSet(resultSet, o);
+
+        ps.close();
+
+
+        return  o;
+
+
+    }
+
+
+    private Object writeResultSet(ResultSet resultSet, Object instancia) throws Exception {
+        try{
+            int contador = 1 ;
+            int p = 0;
+
+//        if (resultSet!=null && resultSet.next()) {
+            //else
+
+            ResultSetMetaData rsmd = resultSet.getMetaData();
+            int nCols = rsmd.getColumnCount();
+
+            System.out.println(rsmd.getColumnCount());
+            System.out.println(rsmd.getColumnName(1));
+            System.out.println(rsmd.getColumnName(2));
+            System.out.println(rsmd.getColumnType(1));
+            System.out.println(rsmd.getColumnType(2));
+
+
+            int i =0;
+            String property = null;
+            while (i < nCols) {
+                // switch
+
+                property = rsmd.getColumnName(i);
+                setter(instancia, property, resultSet.getString(i));
+            }
+
+        /*
+        LinkedList<String> lista = new LinkedList<String>();
+        int i = 1;
+        Field[] fields = instancia.getClass().getDeclaredFields();
+        while(i<= fields.length){
+            lista.add(resultSet.getString(fields[i].getName()));
+            i++;
+        }
+
+
+
+
+        while (p < lista.size() ) {
+
+            String set = fields[p].getName();
+            String set2 = "set"+set;
+            // set.substring(0,1).toUpperCase()+set.substring(1);
+
+            Method m = instancia.getClass().getMethod(set2);
+            m.invoke(instancia, lista.get(p));
+
+            Object[] params = {instancia.getClass().getMethod("Usuario")};
+           Object a = m.invoke(instancia, "guillermo");
+
+
+          }
+*/
+            return instancia;
+
+        }catch (Exception e){
+            System.out.println("no va");
+            throw e;
+        }
+    }
+}
+
+
+
+
+
 
